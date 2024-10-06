e3c30a6ef214d4a54c0a8283a822b1e37d581f28
Steven Troxler <steven.troxler@gmail.com>
2024-10-06 09:32:26 -0700

    Make a trox-nvim-starter: nvim-starter plus ocaml-lsp and color themes

---
+++ trox-nvim-starter/README.md
@@ -0,0 +1,168 @@
+# Neovim Starter
+
+## Source
+
+This config is based the nvim-starter config at
+[the 04-lsp-installer branch][https://github.com/VonHeikemen/nvim-starter/tree/04-lsp-installer]
+of Heikemen's awesome `nvim-starter` project. It is a single-init-lua starter
+that includes basic plugins with LSP support.
+
+I've modified it to:
+- set up `ocamllsp` as a language server. There are two ocaml language server options, this
+  is the one you want and it corresponds to the `ocaml-lsp` opam / nix package, which provides
+  the `ocaml-language-server` executable (yes, the inconsistent naming is a pain).
+- install more color themes, and eliminate a few I don't like. In particular, even though
+  I actually like the color palette in tokyonight (the default in `nvim-starter`) the actual syntax
+  highlighting in OCaml is difficult for me to read.
+
+## Introduction
+
+Base configuration for Neovim. For those looking to make Neovim their new main editor. Here you'll find a popular combination of plugin to make your experience a little bit better.
+
+A few things have been configured to resemble other modern text editors. You'll find a file explorer with tree style view, list open files in tabs, git integration and a terminal easy to toggle.
+
+Autocompletion and "code intellisense" is provided by the LSP client built into Neovim. The following language servers are configured:
+
+* ocaml
+
+Most of the code in this configuration is explained in this series:
+
+* [Build your first Neovim configuration in lua](https://vonheikemen.github.io/devlog/tools/build-your-first-lua-config-for-neovim/)
+* [Neovim: Plugins to get started](https://vonheikemen.github.io/devlog/tools/neovim-plugins-to-get-started/)
+* [Setup nvim-lspconfig + nvim-cmp](https://vonheikemen.github.io/devlog/tools/setup-nvim-lspconfig-plus-nvim-cmp/)
+
+## Requirements
+
+* Neovim v0.8 or greater.
+* git.
+* A `C` compiler. Can be `gcc`, `tcc` or `zig`.
+* [make](https://www.gnu.org/software/make/), the build tool.
+* [tsserver](https://github.com/theia-ide/typescript-language-server). Typescript language server.
+* [vscode-langservers-extracted](https://github.com/hrsh7th/vscode-langservers-extracted). Provides language servers for `eslint`, `css` and `html`.
+* [nodejs](https://nodejs.org/es/). Javascript runtime. Required by the language servers.
+* (optional) [ripgrep](https://github.com/BurntSushi/ripgrep). Improves project wide search speed.
+* (optional) [fd](https://github.com/sharkdp/fd). Improves file search speed.
+* (optional) A patched font to display icons. I hear [nerdfonts](https://www.nerdfonts.com/) has a good collection.
+
+> Here's the config [compatible with v0.7](https://github.com/VonHeikemen/nvim-starter/tree/112bbd38675d35d1c0dbf4e4c4dc750b836c8794). 
+
+## Installation
+
+* Backup your existing configuration if you have one.
+
+* Create an `init.lua` file in your system. Use this command if you don't know the specific location of Neovim's configuration folder.
+
+```sh
+nvim --headless -c 'call mkdir(stdpath("config"), "p") | exe "edit" stdpath("config") . "/init.lua" | write | quit'
+```
+
+* Open your configuration file with Neovim.
+
+```sh
+nvim -c 'edit $MYVIMRC'
+```
+
+* Copy the content of `init.lua` in this repository into your own `init.lua`.
+
+* Next time you start Neovim all plugins will be downloaded automatically. After plugins are downloaded restart Neovim.
+
+### Plugins directory
+
+Your plugins will be installed in a separate directory from your configuration. The location of this directory depends on your operating system and environment variables, so you'll need to execute this command to know where that is.
+
+```sh
+nvim --headless -c 'echo stdpath("data") . "/lazy/lazy.nvim" | quit'
+```
+
+## Keybindings
+
+Leader key: `Space`.
+
+| Mode | Key | Action |
+| --- | --- | --- |
+| Normal | `<leader>h` | Go to first non empty character in line. |
+| Normal | `<leader>l` | Go to last non empty character in line. |
+| Normal | `<leader>a` | Select all text. |
+| Normal | `gy` | Copy selected text to clipboard. |
+| Normal | `gp` | Paste clipboard content. |
+| Normal | `<leader>w` | Save file. |
+| Normal | `<leader>bq` | Close current buffer. |
+| Normal | `<leader>bc` | Close current buffer while preserving the window layout. |
+| Normal | `<leader>bl` | Go to last active buffer. |
+| Normal | `<leader>?` | Search oldfiles history. |
+| Normal | `<leader><space>` | Search open buffers. |
+| Normal | `<leader>ff` | Find file in current working directory. |
+| Normal | `<leader>fg` | Search pattern in current working directory. Interactive "grep search". |
+| Normal | `<leader>fd` | Search diagnostics in current file. |
+| Normal | `<leader>fs` | Search pattern in current file. |
+| Normal | `<leader>e` | Open file explorer. |
+| Normal | `<Ctrl-g>` | Toggle the builtin terminal. |
+| Normal | `K` | Displays hover information about the symbol under the cursor. |
+| Normal | `gd` | Jump to the definition. |
+| Normal | `gD` | Jump to declaration. |
+| Normal | `gi` | Lists all the implementations for the symbol under the cursor. |
+| Normal | `go` | Jumps to the definition of the type symbol |
+| Normal | `gr` | Lists all the references. |
+| Normal | `gs` | Displays a function's signature information. |
+| Normal | `<F2>` | Renames all references to the symbol under the cursor. |
+| Normal | `<F3>` | Format code in current buffer. |
+| Normal | `<F4>` | Selects a code action available at the current cursor position. |
+| Visual | `<F4>` | Selects a code action available in the selected text. |
+| Normal | `gl` | Show diagnostics in a floating window. |
+| Normal | `[d` | Move to the previous diagnostic. |
+| Normal | `]d` | Move to the next diagnostic. |
+
+### Autocomplete keybindings
+
+| Mode | Key | Action |
+| --- | --- | --- |
+| Insert | `<Up>` | Move to previous item. |
+| Insert | `<Down>` | Move to next item. |
+| Insert | `<Ctrl-p>` | Move to previous item. |
+| Insert | `<Ctrl-n>` | Move to next item. |
+| Insert | `<Ctrl-u>` | Scroll up in documentation window. |
+| Insert | `<Ctrl-d>` | Scroll down in documentation window. |
+| Insert | `<Ctrl-e>` | Cancel completion. |
+| Insert | `<C-y>` | Confirm completion. |
+| Insert | `<Enter>` | Confirm completion. |
+| Insert | `<Ctrl-f>` | Go to next placeholder in snippet. |
+| Insert | `<Ctrl-b>` | Go to previous placeholder in snippet. |
+| Insert | `<Tab>` | If completion menu is open, go to next item. Else, open completion menu. |
+| Insert | `<Shift-Tab>` | If completion menu is open, go to previous item. |
+
+## Plugin list
+
+| Name | Description  |
+| --- | --- |
+| [lazy.nvim](https://github.com/folke/lazy.nvim) | Plugin manager. |
+| [tokyonight.nvim](https://github.com/folke/tokyonight.nvim) | Collection of colorscheme for Neovim. |
+| [onedark.vim](https://github.com/joshdick/onedark.vim) | Colorscheme based on Atom's default theme. |
+| [monokai.nvim](https://github.com/tanvirtin/monokai.nvim) | Colorscheme based on Sublime text's default theme. |
+| [darkplus.nvim](https://github.com/lunarvim/darkplus.nvim) | Colorscheme based on VS Code's default theme. |
+| [nvim-web-devicons](https://github.com/kyazdani42/nvim-web-devicons) | Helper functions to show icons. |
+| [lualine.nvim](https://github.com/nvim-lualine/lualine.nvim) | Pretty statusline. |
+| [bufferline.nvim](https://github.com/akinsho/bufferline.nvim) | Pretty tabline. |
+| [indent-blankline.nvim](https://github.com/lukas-reineke/indent-blankline.nvim) | Shows indent guides in current file. |
+| [nvim-tree.lua](https://github.com/kyazdani42/nvim-tree.lua) | File explorer. |
+| [telescope.nvim](https://github.com/nvim-telescope/telescope.nvim) | Fuzzy finder. |
+| [telescope-fzf-native.nvim](https://github.com/nvim-telescope/telescope-fzf-native.nvim) | Extension for telescope. Allows fzf-like syntax in searches. |
+| [gitsigns.nvim](https://github.com/lewis6991/gitsigns.nvim) | Shows indicators in gutter based on file changes detected by git. |
+| [vim-fugitive](https://github.com/tpope/vim-fugitive) | Git integration into Neovim/Vim. |
+| [nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter) | Configures treesitter parsers. Provides modules to manipulate code. |
+| [nvim-treesitter-textobjects](https://github.com/nvim-treesitter/nvim-treesitter-textobjects) | Creates textobjects based on treesitter queries. |
+| [Comment.nvim](https://github.com/numToStr/Comment.nvim) | Toggle comments. |
+| [vim-surround](https://github.com/tpope/vim-surround) | Add, remove, change "surroundings". |
+| [targets.vim](https://github.com/wellle/targets.vim) | Creates new textobjects. |
+| [vim-repeat](https://github.com/tpope/vim-repeat) | Add "repeat" support for plugins. |
+| [vim-bbye](https://github.com/moll/vim-bbye) | Close buffers without closing the current window. |
+| [plenary.nvim](https://github.com/nvim-lua/plenary.nvim) | Collection of modules. Used internaly by other plugins. |
+| [toggleterm.nvim](https://github.com/akinsho/toggleterm.nvim) | Manage terminal windows easily. |
+| [nvim-lspconfig](https://github.com/neovim/nvim-lspconfig) | Quickstart configs for Neovim's LSP client.  |
+| [nvim-cmp](https://github.com/hrsh7th/nvim-cmp) | Autocompletion engine. |
+| [cmp-buffer](https://github.com/hrsh7th/cmp-buffer) | nvim-cmp source. Suggest words in the current buffer. |
+| [cmp-path](https://github.com/hrsh7th/cmp-path) | nvim-cmp source. Show suggestions based on file system paths. |
+| [cmp_luasnip](https://github.com/saadparwaiz1/cmp_luasnip) | nvim-cmp source. Show suggestions based on installed snippets. |
+| [cmp-nvim-lsp](https://github.com/hrsh7th/cmp-nvim-lsp) | nvim-cmp source. Show suggestions based on LSP servers queries. |
+| [LuaSnip](https://github.com/L3MON4D3/LuaSnip) | Snippet engine. |
+| [friendly-snippets](https://github.com/rafamadriz/friendly-snippets) | Collection of snippets. |
+
---
+++ trox-nvim-starter/init.lua
@@ -0,0 +1,551 @@
+-- ========================================================================== --
+-- ==                           EDITOR SETTINGS                            == --
+-- ========================================================================== --
+
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+vim.opt.signcolumn = 'yes'
+
+-- Space as leader key
+vim.g.mapleader = ' '
+
+-- Shortcuts
+vim.keymap.set({'n', 'x', 'o'}, '<leader>h', '^')
+vim.keymap.set({'n', 'x', 'o'}, '<leader>l', 'g_')
+vim.keymap.set('n', '<leader>a', ':keepjumps normal! ggVG<cr>')
+
+-- Basic clipboard interaction
+vim.keymap.set({'n', 'x'}, 'gy', '"+y') -- copy
+vim.keymap.set({'n', 'x'}, 'gp', '"+p') -- paste
+
+-- Delete text
+vim.keymap.set({'n', 'x'}, 'x', '"_x')
+vim.keymap.set({'n', 'x'}, 'X', '"_d')
+
+-- Commands
+vim.keymap.set('n', '<leader>w', '<cmd>write<cr>')
+vim.keymap.set('n', '<leader>bq', '<cmd>bdelete<cr>')
+vim.keymap.set('n', '<leader>bl', '<cmd>buffer #<cr>')
+
+
+-- ========================================================================== --
+-- ==                               COMMANDS                               == --
+-- ========================================================================== --
+
+vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', {})
+
+local group = vim.api.nvim_create_augroup('user_cmds', {clear = true})
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight on yank',
+  group = group,
+  callback = function()
+    vim.highlight.on_yank({higroup = 'Visual', timeout = 200})
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = {'help', 'man'},
+  group = group,
+  command = 'nnoremap <buffer> q <cmd>quit<cr>'
+})
+
+
+-- ========================================================================== --
+-- ==                               PLUGINS                                == --
+-- ========================================================================== --
+
+local lazy = {}
+
+function lazy.install(path)
+  if not vim.loop.fs_stat(path) then
+    print('Installing lazy.nvim....')
+    vim.fn.system({
+      'git',
+      'clone',
+      '--filter=blob:none',
+      'https://github.com/folke/lazy.nvim.git',
+      '--branch=stable', -- latest stable release
+      path,
+    })
+  end
+end
+
+function lazy.setup(plugins)
+  if vim.g.plugins_ready then
+    return
+  end
+
+  -- You can "comment out" the line below after lazy.nvim is installed
+  lazy.install(lazy.path)
+
+  vim.opt.rtp:prepend(lazy.path)
+
+  require('lazy').setup(plugins, lazy.opts)
+  vim.g.plugins_ready = true
+end
+
+lazy.path = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+lazy.opts = {}
+
+lazy.setup({
+  -- Colors
+	{'AlexvZyl/nordic.nvim'},
+	{'neanias/everforest-nvim'},
+	{'catppuccin/nvim'},
+	{'maxmx03/solarized.nvim'},
+	{'jnurmine/zenburn'},
+	{'haishanh/night-owl.vim'},
+	{'alessandroyorba/alduin'},
+	{'romainl/apprentice'},
+	{'sho-87/kanagawa-paper.nvim'},
+	{'xero/miasma.nvim'},
+	{'ilof2/posterpole.nvim'},
+	{'sainnhe/gruvbox-material'},
+	{'tsax/stellarized'},
+
+	-- Appearance
+  {'kyazdani42/nvim-web-devicons'},
+  {'nvim-lualine/lualine.nvim'},
+  {'akinsho/bufferline.nvim'},
+  {'lukas-reineke/indent-blankline.nvim', version = '3.x'},
+
+  -- File explorer
+  {'kyazdani42/nvim-tree.lua'},
+
+  -- Fuzzy finder
+  {'nvim-telescope/telescope.nvim', branch = '0.1.x'},
+  {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'},
+
+  -- Git
+  {'lewis6991/gitsigns.nvim'},
+  {'tpope/vim-fugitive'},
+
+  -- Code manipulation
+  {'nvim-treesitter/nvim-treesitter'},
+  {'nvim-treesitter/nvim-treesitter-textobjects'},
+  {'numToStr/Comment.nvim'},
+  {'tpope/vim-surround'},
+  {'wellle/targets.vim'},
+  {'tpope/vim-repeat'},
+
+  -- Utilities
+  {'moll/vim-bbye'},
+  {'nvim-lua/plenary.nvim'},
+  {'akinsho/toggleterm.nvim'},
+
+  -- LSP support
+  {'neovim/nvim-lspconfig'},
+
+  -- Autocomplete
+  {'hrsh7th/nvim-cmp'},
+  {'hrsh7th/cmp-buffer'},
+  {'hrsh7th/cmp-path'},
+  {'saadparwaiz1/cmp_luasnip'},
+  {'hrsh7th/cmp-nvim-lsp'},
+
+  -- Snippets
+  {'L3MON4D3/LuaSnip'},
+  {'rafamadriz/friendly-snippets'},
+})
+
+
+-- ========================================================================== --
+-- ==                         PLUGIN CONFIGURATION                         == --
+-- ========================================================================== --
+
+---
+-- Colorscheme
+---
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('everforest')
+
+
+---
+-- vim-bbye
+---
+vim.keymap.set('n', '<leader>bc', '<cmd>Bdelete<CR>')
+
+
+---
+-- lualine.nvim (statusline)
+---
+vim.opt.showmode = false
+
+-- See :help lualine.txt
+require('lualine').setup({
+  options = {
+    theme = 'tokyonight',
+    icons_enabled = true,
+    component_separators = '|',
+    section_separators = '',
+    disabled_filetypes = {
+      statusline = {'NvimTree'},
+    },
+  },
+})
+
+
+---
+-- bufferline
+---
+-- See :help bufferline-settings
+require('bufferline').setup({
+  options = {
+    mode = 'buffers',
+    offsets = {
+      {filetype = 'NvimTree'}
+    },
+  },
+  -- :help bufferline-highlights
+  highlights = {
+    buffer_selected = {
+      italic = false
+    },
+    indicator_selected = {
+      fg = {attribute = 'fg', highlight = 'Function'},
+      italic = false
+    }
+  }
+})
+
+
+---
+-- Treesitter
+---
+-- See :help nvim-treesitter-modules
+require('nvim-treesitter.configs').setup({
+  highlight = {
+    enable = true,
+  },
+  -- :help nvim-treesitter-textobjects-modules
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
+      }
+    },
+  },
+  ensure_installed = {
+    'javascript',
+    'typescript',
+    'tsx',
+    'lua',
+    'vim',
+    'vimdoc',
+    'css',
+    'json'
+  },
+})
+
+
+---
+-- Comment.nvim
+---
+require('Comment').setup({})
+
+
+---
+-- Indent-blankline
+---
+-- See :help ibl.setup()
+require('ibl').setup({
+  enabled = true,
+  scope = {
+    enabled = false,
+  },
+  indent = {
+    char = '▏',
+  },
+})
+
+
+---
+-- Gitsigns
+---
+-- See :help gitsigns-usage
+require('gitsigns').setup({
+  signs = {
+    add = {text = '▎'},
+    change = {text = '▎'},
+    delete = {text = '➤'},
+    topdelete = {text = '➤'},
+    changedelete = {text = '▎'},
+  }
+})
+
+
+---
+-- Telescope
+---
+-- See :help telescope.builtin
+vim.keymap.set('n', '<leader>?', '<cmd>Telescope oldfiles<cr>')
+vim.keymap.set('n', '<leader><space>', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>fd', '<cmd>Telescope diagnostics<cr>')
+vim.keymap.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
+
+require('telescope').load_extension('fzf')
+
+
+---
+-- nvim-tree (File explorer)
+---
+-- See :help nvim-tree-setup
+require('nvim-tree').setup({
+  hijack_cursor = false,
+  on_attach = function(bufnr)
+    local bufmap = function(lhs, rhs, desc)
+      vim.keymap.set('n', lhs, rhs, {buffer = bufnr, desc = desc})
+    end
+
+    -- :help nvim-tree.api
+    local api = require('nvim-tree.api')
+
+    -- default mappings
+    api.config.mappings.default_on_attach(bufnr)
+
+    bufmap('L', api.node.open.edit, 'Expand folder or go to file')
+    bufmap('H', api.node.navigate.parent_close, 'Close parent folder')
+    bufmap('gh', api.tree.toggle_hidden_filter, 'Toggle hidden files')
+  end
+})
+
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
+
+
+---
+-- toggleterm
+---
+-- See :help toggleterm-roadmap
+require('toggleterm').setup({
+  open_mapping = '<C-g>',
+  direction = 'horizontal',
+  shade_terminals = true
+})
+
+
+---
+-- Luasnip (snippet engine)
+---
+-- See :help luasnip-loaders
+require('luasnip.loaders.from_vscode').lazy_load()
+
+
+---
+-- nvim-cmp (autocomplete)
+---
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+local select_opts = {behavior = cmp.SelectBehavior.Select}
+
+-- See :help cmp-config
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end
+  },
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'buffer', keyword_length = 3},
+    {name = 'luasnip', keyword_length = 2},
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
+  },
+  formatting = {
+    fields = {'menu', 'abbr', 'kind'},
+    format = function(entry, item)
+      local menu_icon = {
+        nvim_lsp = 'λ',
+        luasnip = '⋗',
+        buffer = 'Ω',
+        path = '🖫',
+      }
+
+      item.menu = menu_icon[entry.source.name]
+      return item
+    end,
+  },
+  -- See :help cmp-mapping
+  mapping = {
+    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+
+    ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+    ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-y>'] = cmp.mapping.confirm({select = true}),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    ['<C-f>'] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(1) then
+        luasnip.jump(1)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+
+    ['<C-b>'] = cmp.mapping(function(fallback)
+      if luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      local col = vim.fn.col('.') - 1
+
+      if cmp.visible() then
+        cmp.select_next_item(select_opts)
+      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        fallback()
+      else
+        cmp.complete()
+      end
+    end, {'i', 's'}),
+
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item(select_opts)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+  },
+})
+
+
+---
+-- Diagnostic customization
+---
+local sign = function(opts)
+  -- See :help sign_define()
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({name = 'DiagnosticSignError', text = '✘'})
+sign({name = 'DiagnosticSignWarn', text = '▲'})
+sign({name = 'DiagnosticSignHint', text = '⚑'})
+sign({name = 'DiagnosticSignInfo', text = '»'})
+
+-- See :help vim.diagnostic.config()
+vim.diagnostic.config({
+  virtual_text = false,
+  severity_sort = true,
+  float = {
+    border = 'rounded',
+    source = 'always',
+  },
+})
+
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {border = 'rounded'}
+)
+
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {border = 'rounded'}
+)
+
+---
+-- LSP Keybindings
+---
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = group,
+  desc = 'LSP actions',
+  callback = function()
+    local bufmap = function(mode, lhs, rhs)
+      local opts = {buffer = true}
+      vim.keymap.set(mode, lhs, rhs, opts)
+    end
+
+    -- You can search each function in the help page.
+    -- For example :help vim.lsp.buf.hover()
+
+    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+    bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+    bufmap({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>')
+    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+
+    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+    bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+
+    -- if using Neovim v0.8 uncomment this
+    -- bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+  end
+})
+
+
+---
+-- LSP servers
+---
+
+local lspconfig = require('lspconfig')
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+vim.lsp.handlers["textDocument/diagnostic"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_diagnostic, {
+    -- Enable underline, use default values
+    underline = true,
+    -- Enable virtual text, override spacing to 4
+    virtual_text = {
+      spacing = 4,
+    },
+    -- Use a function to dynamically turn signs off
+    -- and on, using buffer local variables
+    signs = function(namespace, bufnr)
+      return vim.b[bufnr].show_signs == true
+    end,
+    -- Disable a feature
+    update_in_insert = false,
+  }
+)
+
+-- Prevent multiple instance of lsp servers
+-- if file is sourced again
+if vim.g.lsp_setup_ready == nil then
+  vim.g.lsp_setup_ready = true
+
+  -- See :help lspconfig-setup
+  lspconfig.ocamllsp.setup({capabilities = lsp_capabilities,})
+end
+
