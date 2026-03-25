f5ca726bf47e034c4cd671e14584f571358de75c
Steven Troxler <steven.troxler@gmail.com>
2026-03-25 15:39:31 -0700

    Include chemacs profiles

---
+++ chemacs2-profiles.el
@@ -0,0 +1,4 @@
+(("default"  . ((user-emacs-directory . "~/.config/doomemacs")))
+ ("doom-a"   . ((user-emacs-directory . "~/.config/doomemacs-a")
+                 (env . (("DOOMDIR" . "~/.config/doom-a")))))
+ ("runemacs" . ((user-emacs-directory . "~/.config/runemacs"))))
